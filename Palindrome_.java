818aa605fb0f1a292b75f062a1aa4c3261112811
Tushankar saha <146471775+Tushankar@users.noreply.github.com>
2024-03-03 10:40:24 +0000

    Palindromic array

---
+++ Palindrome_.java
@@ -0,0 +1,23 @@
+
+
+public class Palindrome_{
+    public static void main(String[] args) {
+        int arr[] = {1,2,3,2,1};
+        int flag = 0;
+
+        int n = arr.length;
+
+        for(int i = 0; i<n/2; i++){
+          
+            if(arr[i] != arr[n-i-1]){
+                flag =1;
+                System.out.println("Not a palindromic array");
+                break;
+            }
+        }
+
+        if(flag == 0){
+            System.out.println("Palindromic array");
+        }
+    }
+}
