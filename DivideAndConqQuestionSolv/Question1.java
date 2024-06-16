af404c95a4dbabbbabdc41940c0260d1a08f8a08
Tushankar saha <146471775+Tushankar@users.noreply.github.com>
2024-06-16 18:24:00 +0000

    dd

---
+++ DivideAndConqQuestionSolv/Question1.java
@@ -0,0 +1,38 @@
+package DivideAndConqQuestionSolv;
+
+public class Question1 {
+    public static void sortArr(int []arr, int n){
+        if(n<=1){
+            return;
+        }
+        
+        int x = -1;
+        int y = -1;
+        int prev = arr[0];
+        for(int i=0; i<n; i++){
+            if(prev>arr[i]){
+                if(x==-1){
+                    x = i-1;
+                    y = i;
+                }
+                else{
+                    y = i;
+                }
+            }
+            prev = arr[i];
+        }
+        //Swapping
+        int temp = arr[x];
+        arr[x] = arr[y];
+        arr[y] = temp;
+    }
+    public static void main(String[] args) {
+        int []arr = {3,8,6,7,5,9,10};
+        int n = arr.length;
+        sortArr(arr,n);
+
+        for(int i=0;i<n;i++){
+            System.out.println(arr[i] + " ");
+        }
+    }
+}
